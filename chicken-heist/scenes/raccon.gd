a3ab533479653482974b1fe2316faa0c70702132
Marcus Rosell <marcus.rosell@solme.se>
2026-01-30 22:09:05 +0100

    New aiming

--- chicken-heist/scenes/raccon.gd
+++ chicken-heist/scenes/raccon.gd
@@ -5,6 +5,10 @@ var aim_vector : Vector2
 
 var shot_to := Vector2.ZERO
 
+@export var aim_distance := 300.0
+@export var aim_speed := 500.0
+@export var aim_retract_speed := 10.0
+
 func _ready() -> void:
 	aim_vector = Vector2(1, 0)
 
@@ -12,16 +16,32 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 
 	var new_aim_vector = Input.get_vector("lasso_left", "lasso_right", "lasso_up", "lasso_down")
-	if new_aim_vector.length() > 0:
-		aim_vector = new_aim_vector
+
+#	if new_aim_vector.length() > 0:
+#		aim_vector = new_aim_vector
 
 	aim_vector = aim_vector.normalized()
-	var shoot_vector = aim_vector * 300
-	aim.global_position = self.global_position + shoot_vector
+#	var shoot_vector = aim_vector * 300
+#	aim.global_position = self.global_position + shoot_vector
+
+	var aim_vector = aim.global_position - self.global_position
+	aim_vector += new_aim_vector * aim_speed * delta
+
+	var aim_vector_length = aim_vector.length()
+	if aim_vector_length > aim_distance:
+		aim_vector_length = aim_distance;
+
+	if new_aim_vector.length() == 0:
+		aim_vector_length -= aim_retract_speed
+
+	if(aim_vector_length < 0) :
+		aim_vector_length = 0
+
+	aim.global_position = self.global_position + aim_vector.normalized() *  aim_vector_length
 
 #	line_2d.set_point_position(0, self.global_position)
 #	line_2d.set_point_position(1, self.global_position + shot_to)
 
 	if Input.is_action_just_pressed("lasso_fire"):
 		print("fire")
-		shot_to = shoot_vector
+#		shot_to = shoot_vector
