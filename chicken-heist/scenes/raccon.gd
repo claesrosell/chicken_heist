af5c3488a46935179ee211deee6a41616efba23a
Claes Rosell <claes.rosell@gmail.com>
2026-01-31 14:35:34 +0100

    Continued work on the co-op selectable controls

--- chicken-heist/scenes/raccon.gd
+++ chicken-heist/scenes/raccon.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 
-	var new_aim_vector = Input.get_vector("lasso_left", "lasso_right", "lasso_up", "lasso_down")
+	var new_aim_vector = Input.get_vector(Game.rocky_controls.lasso_left, Game.rocky_controls.lasso_right, Game.rocky_controls.lasso_up, Game.rocky_controls.lasso_down)
 
 #	if new_aim_vector.length() > 0:
 #		aim_vector = new_aim_vector
@@ -44,7 +44,7 @@ func _process(delta: float) -> void:
 #	line_2d.set_point_position(0, self.global_position)
 #	line_2d.set_point_position(1, self.global_position + shot_to)
 
-	if Input.is_action_just_pressed("lasso_fire"):
+	if Input.is_action_just_pressed(Game.rocky_controls.lasso_fire):
 		print("fire")
 		if current_pickable != null:
 			Game.modify_score(current_pickable.get_points())
