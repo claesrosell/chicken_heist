a1e99d0c7bbe4ed393f5e36b583cecef2d748b94
Marcus Rosell <marcus.rosell@solme.se>
2026-02-02 19:10:27 +0100

    Changed time to hen to 3 seconds and fixed a bug

--- chicken-heist/scenes/hen_spawner.gd
+++ chicken-heist/scenes/hen_spawner.gd
@@ -3,7 +3,8 @@ extends Node2D
 const HEN = preload("uid://bn6h61u5pr2ou")
 @onready var hens: Node2D = $Hens
 
-var time_to_hen : float = 4
+@export var time_to_hen : float = 3
+var time_to_hen_left : float = time_to_hen
 var target_hen_count = 8
 
 # Called when the node enters the scene tree for the first time.
@@ -12,11 +13,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	time_to_hen -= delta
+	time_to_hen_left -= delta
 
-	if hens.get_child_count() < target_hen_count && time_to_hen <= 0:
+	if hens.get_child_count() < target_hen_count && time_to_hen_left <= 0:
 		var hen_instance = HEN.instantiate() as Hen
 		hens.add_child(hen_instance)
 		hen_instance.global_position = self.global_position
 		hen_instance._set_state(Hen.HenState.Running)
-		time_to_hen = 5
+		time_to_hen_left = time_to_hen
